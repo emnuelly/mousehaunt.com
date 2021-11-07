0178f7cc8ae1c16be380fa4dfac3b0e5277cc26c
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2021-11-07 16:43:10 -0500

    Update config

--- src/config.ts
+++ src/config.ts
@@ -40,6 +40,8 @@ const config = {
     MouseHauntToken: {
       address: "0x5Cb2C3Ed882E37DA610f9eF5b0FA25514d7bc85B",
       owner: "0x8e3f936932AbDa7a4C31EFE83068A00aeB69122B",
+      image: "https://mousehaunt.com/images/logo.png",
+      symbol: "MHT",
     },
     BUSD: {
       address: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
@@ -59,10 +61,16 @@ const config = {
     BMHTL: {
       address: "0x29421DF4FEAA2ff2eA08b7A8F221425C8aFD424C",
       owner: "0x2124b4912532f6cD235081fEA2223EB3C0Af301d",
+      image:
+        "https://nft.mousehaunt.com/booster/0000000000000000000000000000000000000000000000000000000000000001/0000000000000000000000000000000000000000000000000000000000000001.png",
+      symbol: "BMHTL",
     },
     BMHTE: {
       address: "0x2DE83F4243ff0200624a44011777a373aF7E9f72",
       owner: "0x2124b4912532f6cD235081fEA2223EB3C0Af301d",
+      image:
+        "https://nft.mousehaunt.com/booster/0000000000000000000000000000000000000000000000000000000000000002/0000000000000000000000000000000000000000000000000000000000000002.png",
+      symbol: "BMHTE",
     },
   },
 };
