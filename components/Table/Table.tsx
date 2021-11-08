635ac77f4da88870384359534196c9fa991d41c5
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2021-11-08 17:28:48 -0500

    Table

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import mht from "../../public/images/MHT.png";
 import coffin from "../../public/images/coffin-store.png";
 
-import { Styles } from "./styles";
+import { Styles, StatusBadge } from "./styles";
 import { StoreContext } from "../../contexts/StoreContext";
 
 const Table: React.FC = () => {
@@ -16,7 +16,7 @@ const Table: React.FC = () => {
       type: userInfo?.boosters.legendary,
       typeSub: "Available on wallet",
       image: coffin,
-      status: 1,
+      status: "AVAILABLE",
     },
     {
       item: "BMHTE",
@@ -24,7 +24,7 @@ const Table: React.FC = () => {
       type: userInfo?.boosters.epic,
       typeSub: "Available on wallet",
       image: coffin,
-      status: 1,
+      status: "AVAILABLE",
     },
     {
       item: "MHT",
@@ -32,68 +32,30 @@ const Table: React.FC = () => {
       type: userInfo?.totalTokens,
       typeSub: "Claimable 1 month after IGO",
       image: mht,
-      status: 2,
+      status: "LOCKED",
     },
   ];
 
-  const styleTypeButtons = (val: number) => {
-    return val === 1 ? (
-      <div
-        style={{
-          backgroundColor: "#374A5F",
-          height: "50px",
-          borderRadius: "10px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          border: "2px solid #458B72",
-        }}
-      >
-        <div style={{ fontFamily: "Bebas Neue Pro", fontSize: "20px" }}>
-          AVAILABLE
-        </div>
-      </div>
-    ) : (
-      <div
-        style={{
-          backgroundColor: "#52345D",
-          height: "50px",
-          borderRadius: "10px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          border: "2px solid #A04766",
-        }}
-      >
-        <div style={{ fontFamily: "Bebas Neue Pro", fontSize: "20px" }}>
-          LOCKED
-        </div>
-      </div>
-    );
-  };
-
   const returnAllTables = () => {
     return data.map((e) => {
-      return (
-        <>
-          <tr
-            style={{
-              textAlign: "center",
-              backgroundColor: "#29274b",
-            }}
-          >
-            <td
-              style={{
-                width: "150px",
-              }}
-            >
-              {<Image alt="MHT" width="100" height="100" src={e.image}></Image>}
+      return <></>;
+    });
+  };
+  return (
+    <Styles>
+      <table>
+        <tr>
+          <th>Item</th>
+          <th></th>
+          <th>Amount</th>
+          <th>Status</th>
+        </tr>
+        {data.map((e) => (
+          <tr key={e.item}>
+            <td>
+              <Image alt="MHT" width="100" height="100" src={e.image} />
             </td>
-            <td
-              style={{
-                textAlign: "left",
-              }}
-            >
+            <td>
               <div
                 style={{
                   fontFamily: "SF Pro Display",
@@ -142,175 +104,16 @@ const Table: React.FC = () => {
                 {e.typeSub}
               </div>
             </td>
-            <td style={{ width: "140px" }}>{styleTypeButtons(e.status)}</td>
+            <td style={{ width: "140px" }}>
+              <StatusBadge status={e.status}>
+                <div>{e.status}</div>
+              </StatusBadge>
+            </td>
           </tr>
-        </>
-      );
-    });
-  };
-  return (
-    <Styles>
-      <table>
-        <tr>
-          <th
-            style={{
-              width: "40px",
-              fontFamily: "SF Pro Display",
-              fontWeight: 400,
-            }}
-          >
-            Item
-          </th>
-          <th></th>
-          <th
-            style={{
-              textAlign: "left",
-              fontFamily: "SF Pro Display",
-              fontWeight: 400,
-            }}
-          >
-            Amount
-          </th>
-          <th
-            style={{
-              fontFamily: "SF Pro Display",
-              fontWeight: 400,
-            }}
-          >
-            Status
-          </th>
-        </tr>
-        {returnAllTables()}
+        ))}
       </table>
     </Styles>
   );
 };
 
-// import {
-//   useTable,
-//   useFilters,
-//   useGlobalFilter,
-//   useAsyncDebounce,
-// } from 'react-table';
-// import { COLUMNS, mock_data } from './columns';
-// useEffect(() => {
-//   amountOfColumns();
-// }, [result]);
-// const amountOfColumns = () => {
-//   return setResult(mock_data.length);
-// };
-
-// const SelectColumnFilter = ({
-//   column: { filterValue, setFilter, preFilteredRows, id },
-// }) => {
-//   const options = React.useMemo(() => {
-//     const options = new Set();
-//     preFilteredRows.forEach(row => {
-//       options.add(row.values[id]);
-//     });
-//     return [...options.values()];
-//   }, [id, preFilteredRows]);
-//   return (
-//     <>
-//       <select
-//         value={filterValue}
-//         onChange={e => {
-//           setFilter(e.target.value || undefined);
-//         }}
-//       >
-//         <option value="">TYPE</option>
-//         {options.map((option, i) => {
-//           return (
-//             <option key={i} value={option}>
-//               {option}
-//             </option>
-//           );
-//         })}
-//       </select>
-//     </>
-//   );
-// };
-
-// const moveRow = (dragIndex, hoverIndex) => {
-//   const dragRecord = records[dragIndex];
-//   setRecords(
-//     update(records, {
-//       $splice: [
-//         [dragIndex, 1],
-//         [hoverIndex, 0, dragRecord],
-//       ],
-//     })
-//   );
-// };
-
-// const columns = useMemo(() => COLUMNS(SelectColumnFilter), []);
-// const data = useMemo(() => mock_data, []);
-
-// const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-//   useTable(
-//     {
-//       columns,
-//       data,
-//     },
-//     useFilters,
-//     useGlobalFilter
-//   );
-
-// return (
-//   <>
-//     <div
-//       style={{
-//         display: 'flex',
-//         justifyContent: 'space-around',
-//         marginTop: '40px',
-//       }}
-//     >
-//       {headerGroups.map(headerGroup => (
-//         <div {...headerGroup.getHeaderGroupProps()}>
-//           {headerGroup.headers.map(column => (
-//             <div {...column.getHeaderProps()}>
-//               <div>{column.canFilter ? column.render('Filter') : null}</div>
-//             </div>
-//           ))}
-//         </div>
-//       ))}
-
-//       <StyleResults>
-//         RESULTS:
-//         {result}
-//       </StyleResults>
-//     </div>
-//     <Styles>
-//       <table {...getTableProps()}>
-//         <thead>
-//           {headerGroups.map(headerGroup => (
-//             <tr {...headerGroup.getHeaderGroupProps()}>
-//               {headerGroup.headers.map(column => (
-//                 <th {...column.getHeaderProps()}>
-//                   {column.render('Header')}
-//                 </th>
-//               ))}
-//             </tr>
-//           ))}
-//         </thead>
-//         <tbody {...getTableBodyProps()}>
-//           {rows.map(row => {
-//             prepareRow(row);
-//             return (
-//               <tr {...row.getRowProps()}>
-//                 {row.cells.map(cell => {
-//                   return cell?.row?.original?.status === 'DEACTIVATED' ? (
-//                     <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-//                   ) : (
-//                     <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-//                   );
-//                 })}
-//               </tr>
-//             );
-//           })}
-//         </tbody>
-//       </table>
-//     </Styles>
-//   </>
-// );
 export default Table;
--- components/Table/styles.ts
+++ components/Table/styles.ts
@@ -16,8 +16,17 @@ width: 100%;
         }
       }
     }
-    th{
+    th {
       color: #848484;
+      text-align: left;
+      font-family: "SF Pro Display";
+      font-weight: 400;
+    }
+    th:nth-child(1) {
+      width: 40px;
+      text-align: center;
+    }
+    th:nth-child(4) {
       text-align: center;
     }
 
@@ -29,11 +38,42 @@ width: 100%;
       &:last-child {
         border-right: 0;
       }
+      div {
+        font-family: "SF Pro Display";
+        font-weight: 600;
+        font-size: 24;
+        text-transform: uppercase;
+      }
     }
 
     tr {
       border-radius: 12px;
-
+      background-color: #29274b;
+      td {
+        min-width: 150px;
+      }
+    }
+    tr:nth-child(1) {
+      text-align: center;
+      background-color:unset;
     }
   }
 `;
+
+interface Props {
+  status: string
+}
+export const StatusBadge = styled.div<Props>`
+  background-color: ${props => props.status === 'AVAILABLE' ? "#374A5F" : "#52345D"};
+  height: 50px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: ${props => props.status === 'AVAILABLE' ? "2px solid #458B72" : "2px solid #A04766"};
+  margin-right: 10px;
+  div {
+  font-family: "Bebas Neue Pro";
+  font-size: 20px; 
+  }
+`
