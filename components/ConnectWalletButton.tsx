4770f7bd3576f087486b3a3da809c561deac48e6
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2021-11-08 07:29:06 -0500

    Display minimum info

--- components/ConnectWalletButton.tsx
+++ components/ConnectWalletButton.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { useContracts } from "../hooks/useContracts";
 import { Button } from "./Button";
 
 const Container = styled.div`
@@ -10,25 +12,83 @@ const Container = styled.div`
   }
 `;
 
+const WalletInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: right;
+  pre {
+    margin: 5px 0px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+
+    span {
+      margin-left: 10px;
+    }
+  }
+`;
+
 export const ConnectWalletButton = () => {
   const [account, setAccount] = useState("");
+  const [whitelisted, setWhitelisted] = useState(false);
+  const [mhtAmount, setMhtAmount] = useState("");
+  const contracts = useContracts();
+
+  const getAccount = async () => {
+    const accounts = await window.ethereum?.request({
+      method: "eth_requestAccounts",
+    });
+    return accounts ? (accounts as string[])[0] : "";
+  };
 
   const onClick = async () => {
     if (account) {
       setAccount("");
     } else {
-      const accounts = (await window.ethereum?.request({
-        method: "eth_requestAccounts",
-      })) as string[];
-      setAccount(accounts[0]);
+      setAccount(await getAccount());
     }
   };
+
+  useEffect(() => {
+    window.ethereum?.on("accountsChanged", function (accounts) {
+      setAccount((accounts as string[])[0]);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (account) {
+      (async () => {
+        const isWhitelisted = await contracts.whitelistSale.isWhitelisted(
+          account
+        );
+        setWhitelisted(isWhitelisted);
+        const [totalAmountInWei] =
+          await contracts.whitelistSale.addressToUserInfo(account);
+        const totalAmount = ethers.utils.formatEther(totalAmountInWei);
+        setMhtAmount(totalAmount);
+      })();
+    }
+  }, [account, contracts]);
 
   const text = account ? "DISCONNECT" : "CONNECT TO WALLET";
 
   return (
     <Container>
-      <pre>{account}</pre>
+      <WalletInfo>
+        <pre>{account}</pre>
+        <div>
+          <span>
+            {!account ? "" : whitelisted ? "WHITELISTED" : "NOT WHITELISTED"}
+          </span>
+          <span>
+            {account && whitelisted ? "| " + mhtAmount + " MHT PURCHASED" : ""}
+          </span>
+        </div>
+      </WalletInfo>
       <Button onClick={onClick}>{text}</Button>
     </Container>
   );
---
+++ hooks/useContracts.ts
@@ -0,0 +1,55 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import config from "../utils/config";
+import { WhitelistSale } from "../typechain/WhitelistSale";
+import WhitelistSaleJson from "../contracts/WhitelistSale.sol/WhitelistSale.json";
+import { BoosterSale } from "../typechain/BoosterSale";
+import BoosterSaleJson from "../contracts/booster/BoosterSale.sol/BoosterSale.json";
+import BUSDJson from "../contracts/MouseHauntToken.sol/MouseHauntToken.json";
+import { MouseHauntToken as BUSD } from "../typechain/MouseHauntToken";
+
+export interface Contracts {
+  provider: ethers.providers.Web3Provider;
+  whitelistSale: WhitelistSale;
+  boosterSale: BoosterSale;
+  busd: BUSD
+}
+
+export function useContracts() {
+  const [contracts, setContracts] = useState<Contracts>(null);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+      const signer = provider.getSigner(0);
+      const whitelistSale = new ethers.Contract(
+        config.bscTestnet.WhitelistSale.PrivateSale.address,
+        WhitelistSaleJson.abi,
+        signer
+      ) as WhitelistSale;
+      const boosterSale = new ethers.Contract(
+        config.bscTestnet.BoosterSale.address,
+        BoosterSaleJson.abi,
+        signer
+      ) as BoosterSale;
+      const busd = new ethers.Contract(
+        config.bscTestnet.BUSD.address,
+        BUSDJson.abi,
+        signer
+      ) as BUSD;
+
+      setContracts({
+        provider,
+        whitelistSale,
+        boosterSale,
+        busd
+      });
+
+      return () => {
+        // TODO
+      }
+    }
+  }, []);
+
+  return contracts;
+}
