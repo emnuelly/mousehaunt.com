d7045a44d284e7c01016217ccbae457d0235c385
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2021-11-09 00:00:23 -0500

    Redeploy

--- utils/config.ts
+++ utils/config.ts
@@ -8,6 +8,8 @@ const config = {
     MouseHauntToken: {
       address: "0x034d0272AEbCE737B35C61b3Ab709020cEDca808",
       owner: "0x087B58029f7251E7054153Bc8775e14A68490286",
+      image: "https://mousehaunt.com/images/logo.png",
+      symbol: "MHT",
     },
     BUSD: {
       address: "0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee",
