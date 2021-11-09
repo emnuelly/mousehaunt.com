092a4d415af3932dcea553899065c83109a48df1
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2021-11-09 12:36:31 -0500

    Add Seed Sales ends in

--- components/StorePage/index.tsx
+++ components/StorePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { NextPage } from "next";
 import {
   Container,
@@ -6,13 +6,12 @@ import {
   StoreContainer,
   StorePageSub,
   StoreTitle,
-  StoreInventory,
+  CountdownContainer,
 } from "./styles";
 import Cards from "../Cards";
 import Header from "../Header";
 import Logo from "../Logo";
 import Sections from "../Sections";
-import Table from "../../components/Table/Table";
 import { ConnectWalletButton } from "../ConnectWalletButton";
 
 const sections = [
@@ -20,7 +19,66 @@ const sections = [
   { to: "/store/inventory", title: "Inventory" },
 ];
 
+function countdown() {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const final = new Date("2021-11-14T23:59:59.000Z");
+
+  const countDown = new Date(final).getTime();
+
+  const now = new Date().getTime();
+  const distance = countDown - now;
+
+  if (distance < 0) {
+    document.getElementById("ends")!.innerText = "SEED SALE ENDED";
+    document.getElementById("ended-ul")!.style.display = "none";
+    return;
+  }
+
+  document.getElementById("days")!.innerText = Math.floor(
+    distance / day
+  ).toString();
+  document.getElementById("hours")!.innerText = Math.floor(
+    (distance % day) / hour
+  ).toString();
+  document.getElementById("minutes")!.innerText = Math.floor(
+    (distance % hour) / minute
+  ).toString();
+  document.getElementById("seconds")!.innerText = Math.floor(
+    (distance % minute) / second
+  ).toString();
+}
+
+const Countdown = () => (
+  <CountdownContainer>
+    <span id="ends">SEED SALE ENDS IN</span>
+    <ul id="ended-ul">
+      <li>
+        <span id="days"></span>DAYS
+      </li>
+      <li>
+        <span id="hours"></span>HOURS
+      </li>
+      <li>
+        <span id="minutes"></span>MINUTES
+      </li>
+      <li>
+        <span id="seconds"></span>SECONDS
+      </li>
+    </ul>
+  </CountdownContainer>
+);
+
 const StoreHome: NextPage = () => {
+  useEffect(() => {
+    setInterval(() => {
+      countdown();
+    }, 1000);
+  }, []);
+
   return (
     <Container>
       <Content>
@@ -36,6 +94,7 @@ const StoreHome: NextPage = () => {
             <br />
             Here you will find the latest goods on this cheesy universe!
           </StorePageSub>
+          <Countdown />
           <Cards />
         </StoreContainer>
       </Content>
