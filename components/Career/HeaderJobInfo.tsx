ece181f1de437a0e71e4855d317e0475a19b2b82
Gabriel <gabriel@kyte.com.br>
2021-11-22 13:04:11 -0300

    fixed build error

--- components/Career/HeaderJobInfo.tsx
+++ components/Career/HeaderJobInfo.tsx
@@ -19,27 +19,32 @@ interface Props {
   };
 }
 const HeaderJobInfo: React.FC<Props> = ({ jobListingHeader }: Props) => {
-  const { jobInfo } = jobListingHeader;
-  const keys = 'icon';
-  const icons = [
-    <FiBriefcase key={keys} />,
-    <BsBarChart key={keys} />,
-    <RiMapPinLine key={keys} />,
-    <FiDollarSign key={keys} />,
-  ];
+  const displayInlineHeader = () => {
+    const { jobInfo } = jobListingHeader;
+    const keys = 'icon';
+    const icons = [
+      <FiBriefcase key={keys} />,
+      <BsBarChart key={keys} />,
+      <RiMapPinLine key={keys} />,
+      <FiDollarSign key={keys} />,
+    ];
+    return Object.entries(jobInfo).map((e: any, k: number) => {
+      return (
+        <>
+          <JobDescriptionText key={k}>
+            <IconStyle>
+              {icons[k]}
+              <IconPadding>{e[0]}</IconPadding>
+            </IconStyle>
+            <br />
+            <IconTextSub>{e[1]}</IconTextSub>
+          </JobDescriptionText>
+        </>
+      );
+    });
+  };
 
-  return Object.entries(jobInfo).map((e: any, k: number) => {
-    return (
-      <JobDescriptionText key={k}>
-        <IconStyle>
-          {icons[k]}
-          <IconPadding>{e[0]}</IconPadding>
-        </IconStyle>
-        <br />
-        <IconTextSub>{e[1]}</IconTextSub>
-      </JobDescriptionText>
-    );
-  });
+  return <>{displayInlineHeader()}</>;
 };
 
 export default HeaderJobInfo;
