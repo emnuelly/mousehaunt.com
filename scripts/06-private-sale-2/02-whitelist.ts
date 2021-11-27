a1da85f9116a56dc1af3663900635493bd53d79e
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2021-11-27 01:23:56 -0500

    Deploy private sale

--- scripts/06-private-sale-2/02-whitelist.ts
+++ scripts/06-private-sale-2/02-whitelist.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 // eslint-disable-next-line node/no-missing-import
 import config, { Network } from "../../src/config";
-import whitelist from "../whitelist.json";
+import whitelist2 from "../whitelist-2.json";
 
 async function main() {
   const network: Network = process.env.NETWORK as Network;
@@ -12,8 +12,23 @@ async function main() {
     config[network].WhitelistSale.PrivateSale2.address
   );
 
-  console.log("WhitelistSale whitelist", whitelist);
-  await whitelistSale.addToWhitelist(whitelist.buyers);
+  console.log("WhitelistSale whitelist", whitelist2.length, `entries`);
+
+  const STEP = 200;
+  const SKIP = 0;
+  for (let i = SKIP; i * STEP < whitelist2.length; i++) {
+    const from = i * STEP;
+    const to = (i + 1) * STEP;
+    const wallets = [...whitelist2].slice(from, to);
+
+    console.log(
+      `${from}..${to - 1}\t${whitelist2[from]} ${whitelist2[to - 1]}`
+    );
+
+    const tx = await whitelistSale.addToWhitelist(wallets);
+
+    await tx.wait();
+  }
 }
 
 main().catch((error) => {
