a29c8c051ec90785a075854fe4d56f086be3d3a1
Gabriel Barreto <gh.barreto@hotmail.com>
2021-12-06 00:33:24 -0300

    reorganized sections

--- utils/sections.ts
+++ utils/sections.ts
@@ -1,16 +1,16 @@
 export const sections = [
   { to: '/#about', title: 'About' },
-  { to: '/#Gameplay', title: 'Gameplay' },
+  {
+    to: 'https://whitepaper.mousehaunt.com',
+    title: 'White Paper',
+  },
   { to: '/store', title: 'Store' },
+  { to: '/#Gameplay', title: 'Gameplay' },
   { to: '/#ValueProposition', title: 'Value Proposition' },
   { to: '/#Token', title: 'Token' },
   { to: '/#Team', title: 'Team' },
   { to: '/#Roadmap', title: 'Roadmap' },
   { to: '/careers', title: 'Career' },
-  {
-    to: 'https://whitepaper.mousehaunt.com',
-    title: 'White Paper',
-  },
 ];
 
 export default sections;
