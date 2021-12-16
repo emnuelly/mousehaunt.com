15eee405b78a7198d391f8d008b26ffe4a0b7dd6
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2021-12-16 00:52:49 -0300

    Update presale data

--- utils/bscTestnet/presale.ts
+++ utils/bscTestnet/presale.ts
@@ -1,18 +1,18 @@
 const seedSale = [
   {
-    address: "",
+    address: "0x5FA41E248EfF800FD69e7D048888686BDc7bC6aF",
     whitelisted: "0x343BD4e802BaE35F89e043299B82067aab38dfd3",
-    amount: "20000",
+    amount: "200",
   },
   {
-    address: "",
+    address: "0xFA01D9FdD0169E07e7d4855f2467fc4a1124078C",
     whitelisted: "0xDe15C3D6845D6Cc7A8a8f80aFD6514b0494dA91A",
-    amount: "16000",
+    amount: "160",
   },
 ].map((term) => ({
   ...term,
   owner: "0x087B58029f7251E7054153Bc8775e14A68490286",
-  MHTtoBUSD: "0.125",
+  MHTtoBUSD: "0.00125",
   unlockAtIGOPercent: "4",
   cliffMonths: "0",
   vestingPeriodMonths: "12",
@@ -20,14 +20,14 @@ const seedSale = [
 
 const privateSale = [
   {
-    address: "",
+    address: "0x9eBfb7b87543aAE30c586a8c1508eD1388BdFD7d",
     whitelisted: "0x343BD4e802BaE35F89e043299B82067aab38dfd3",
-    amount: "15000",
+    amount: "150",
   },
 ].map((term) => ({
   ...term,
   owner: "0x087B58029f7251E7054153Bc8775e14A68490286",
-  MHTtoBUSD: "0.15",
+  MHTtoBUSD: "0.0015",
   unlockAtIGOPercent: "8",
   cliffMonths: "0",
   vestingPeriodMonths: "12",
