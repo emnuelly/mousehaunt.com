15593c70c3362faa7765c889595f049837f8cdb0
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2021-12-16 01:13:07 -0300

    Change button to STORE

--- components/common/Sections.tsx
+++ components/common/Sections.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { Link as LinkButton } from './Link';
-import Link from 'next/link';
-import sections, { storeSections } from '../../utils/sections';
-import { useRouter } from 'next/router';
+import { Link as LinkButton } from "./Link";
+import Link from "next/link";
+import sections, { storeSections } from "../../utils/sections";
+import { useRouter } from "next/router";
 
 interface Props {
   mobile?: boolean;
@@ -17,20 +17,20 @@ interface ContainerProps {
 
 const Container = styled.div<ContainerProps>`
   display: flex;
-  flex-direction: ${props => (props.mobile ? 'column' : 'row')};
-  justify-content: ${props => (props.mobile ? 'space-around' : '')};
-  height: ${props => (props.mobile ? '100%' : '')};
+  flex-direction: ${(props) => (props.mobile ? "column" : "row")};
+  justify-content: ${(props) => (props.mobile ? "space-around" : "")};
+  height: ${(props) => (props.mobile ? "100%" : "")};
   align-items: center !important;
-  width: ${props => props.width};
+  width: ${(props) => props.width};
 
   a {
     margin-left: inherit;
-    margin-right: ${props => (props.mobile ? '' : '32px')};
+    margin-right: ${(props) => (props.mobile ? "" : "32px")};
     z-index: 1;
   }
   a:after {
     display: block;
-    content: '';
+    content: "";
     border-bottom: solid 3px #be42d7;
     transform: scaleX(0);
     transition: transform 0.2s ease-in-out;
@@ -41,7 +41,7 @@ const Container = styled.div<ContainerProps>`
   }
 
   @media only screen and (max-width: 600px) {
-    ${props => (props.mobile ? '' : 'display: none')};
+    ${(props) => (props.mobile ? "" : "display: none")};
   }
 
   .right {
@@ -52,21 +52,21 @@ const Container = styled.div<ContainerProps>`
 
 const Sections: React.FC<Props> = ({ mobile }: Props) => {
   const router = useRouter();
-  const isStorePath = router.pathname.includes('/store');
+  const isStorePath = router.pathname.includes("/store");
   const routes = isStorePath ? storeSections : sections;
 
   return (
-    <Container width={isStorePath ? '' : '100%'} mobile={mobile}>
-      {routes.map(section => (
+    <Container width={isStorePath ? "" : "100%"} mobile={mobile}>
+      {routes.map((section) => (
         <Link key={section.to} href={section.to}>
-          <a target={section.title === 'White Paper' ? '_blank' : ''}>
+          <a target={section.title === "White Paper" ? "_blank" : ""}>
             {section.title}
           </a>
         </Link>
       ))}
       {isStorePath ? null : (
         <LinkButton className="right" href="/store">
-          PRIVATE SALE (#3)
+          STORE
         </LinkButton>
       )}
     </Container>
