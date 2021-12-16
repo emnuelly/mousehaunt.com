7a5f826a0257cf195aca51281f8f32ce63dfb86b
Gabriel Barreto <gh.barreto@hotmail.com>
2021-12-16 11:57:41 -0300

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ import Document, {
   Main,
   NextScript,
   DocumentContext,
-} from "next/document";
-import { ServerStyleSheet } from "styled-components";
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -15,8 +15,7 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -88,7 +87,11 @@ export default class MyDocument extends Document {
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="language" content="English" />
           <link rel="icon" href="/favicon.ico" />
-          <link rel="preload" href="/images/game-related/fireplace.webp" as="image" />
+          <link
+            rel="preload"
+            href="/images/game-related/fireplace.webp"
+            as="image"
+          />
           <link
             rel="preload"
             href="https://assets.mousehaunt.com/videos/LandingPage.mp4"
@@ -128,8 +131,9 @@ export default class MyDocument extends Document {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
           />
+
           {/* eslint-disable-next-line @next/next/next-script-for-ga */}
-          <script
+          {/* <script
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -137,7 +141,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer',${process.env.GTM_ID});`,
             }}
-          />
+          /> */}
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','${process.env.GTM_ID}');`,
+            }}
+          ></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -152,12 +166,12 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </Head>
         <body>
-          <noscript
+          {/* <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
-          />
+          /> */}
           <Main />
           <NextScript />
         </body>
